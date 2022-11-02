b2794f112ea6238e7cf75550381e4d57bf25cd6e
YUUJIIE <y-ujiie@outlook.com>
2022-11-02 21:37:33 +0900

    first commit

---
+++ src/staffinfo.java
@@ -0,0 +1,14 @@
+class Staff {
+    String name;
+    int staffid;
+    String email;
+}
+
+public class staffinfo {
+    public static void main(String[]args){
+        Staff yamada = new Staff();
+        yamada.name = "Taro Yamada";
+
+        System.out.println(yamada.name);
+    }
+}
